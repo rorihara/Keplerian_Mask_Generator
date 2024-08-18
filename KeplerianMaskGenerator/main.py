25faceb3d57fa1ca295d5282ea25cb703f14c935
rorihara <100898752+rorihara@users.noreply.github.com>
2024-08-19 00:48:55 +0900

    Update main.py

--- KeplerianMaskGenerator/main.py
+++ KeplerianMaskGenerator/main.py
@@ -9,12 +9,18 @@ class generator:
 
     def generate_mask(self, slice_image):
         self.slice_image = slice_image
-        self.loadfits = LoadFits(self.config, self.source, self.slice_image)
-        self.makemask = MakeMask(self.config, self.source, self.loadfits)
-        return self.loadfits.data, self.makemask.all_masks, self.makemask.u_masks_conv, self.makemask.l_masks_conv
+        self.__loadfits = LoadFits(self.config, self.source, self.slice_image)
+        self.__makemask = MakeMask(self.config, self.source, self.__loadfits)
+        self.data = self.__loadfits.data
+        self.all_mask = self.__makemask.all_masks
+        self.upper_mask = self.__makemask.u_masks_conv
+        self.lower_mask = self.__makemask.l_masks_conv
+        self.ra = self.__loadfits.ra
+        self.dec = self.__loadfits.dec
+        self.vel = self.__loadfits.vel_list
 
     def save_mask(self, save_fits, save_npy, save_animation, vrange=3):
-        masksaver = MaskSaver(self.config, self.source, self.makemask)
+        masksaver = MaskSaver(self.config, self.source, self.__makemask)
         if save_fits:
             if self.slice_image:
                 print("The FITS file will not be saved because the image has been sliced.")
