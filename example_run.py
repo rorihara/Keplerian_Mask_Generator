1ac55d9162deb1d388bb706b7a1db503cde14a6b
rorihara <100898752+rorihara@users.noreply.github.com>
2024-08-18 21:19:20 +0900

    Update example_run.py

--- example_run.py
+++ example_run.py
@@ -1,34 +1,34 @@
 from KeplerianMaskGenerator import generator as KMG
 
 config = {
-        'pd': '/Volumes/Extreme_SSD/obs_data/ALMA/MAPS/HD163296/casa_space/', # Path to parent directory of FITS file
-        'fits':'test.fits', # Name of FITS file
+        'pd': '/path/to/directory/', # Path to parent directory of FITS file
+        'fits':'xxx.fits', # Name of FITS file
         'cf': 1, # Convolution factor
         'same_upperlower': False, # whether to use the same upper and lower surface or not
         }
 
 source = {
-    'name':'HD_163296', # Source name
-    'dpc':101.5, # Distance to the source (pc)
-    'Ms':1.92, # Solar masses (Mo)
-    'Vsys':5.76, # velocity of the system (km/s)
-    'incl':-46.7, # inclination (deg)
-    'pa':312.8, # position angle (deg)
-    'vel_sign':-1, # 1 for clockwise, -1 for counterclockwise
-    'Rout':550, # disk radius (au)
-    'dRA':0.01267, # offset in RA (arcsec)
-    'dDEC':0.00295, # offset in DEC (arcsec)
-    'h0_u':29.6, # scaling factor for the height of the upper surface
-    'p_u':1.29, # power law index of the upper surface
-    'Rb_u':435.4, # break radius of the upper surface (au)
-    'q_u':1.66, # tapering index of the upper surface
-    'h0_l':16.7, # scaling factor for the height of the lower surface (if same_upperlower == False)
-    'p_l':1.16, # power law index of the lower surface (if same_upperlower == False)
-    'Rb_l':559.0, # break radius of the lower surface (au) (if same_upperlower == False)
-    'q_l':3.5, # tapering index of the lower surface (if same_upperlower == False)
-    'L0':0.32, # linewidth at 100 au (km/s)
-    'p':-0.2, # power law index for the linewidth as a function of radius
-    'q':-0.47 # power law index for the linewidth as a function of height
+    'name':'source', # Source name
+    'dpc':100., # Distance to the source (pc)
+    'Ms':1., # Solar masses (Mo)
+    'Vsys':5., # velocity of the system (km/s)
+    'incl':30., # inclination (deg)
+    'pa':30., # position angle (deg)
+    'vel_sign':1, # 1 for clockwise, -1 for counterclockwise
+    'Rout':100, # disk radius (au)
+    'dRA':0., # offset in RA (arcsec)
+    'dDEC':0., # offset in DEC (arcsec)
+    'h0_u':30., # scaling factor for the height of the upper surface
+    'p_u':1., # power law index of the upper surface
+    'Rb_u':300., # break radius of the upper surface (au)
+    'q_u':1., # tapering index of the upper surface
+    'h0_l':30., # scaling factor for the height of the lower surface (if same_upperlower == False)
+    'p_l':1., # power law index of the lower surface (if same_upperlower == False)
+    'Rb_l':300., # break radius of the lower surface (au) (if same_upperlower == False)
+    'q_l':1., # tapering index of the lower surface (if same_upperlower == False)
+    'L0':0.3, # linewidth at 100 au (km/s)
+    'p':-0.5, # power law index for the linewidth as a function of radius
+    'q':-0.5 # power law index for the linewidth as a function of height
     }
 
 # Initialize the generator
